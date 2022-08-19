63d1a1913fbbe7b0963ff74b5d128e1606053911
magmagaming001 <wassimdzd@gmail.com>
2022-08-19 12:36:50 +0100

    tried changing ip setings

--- src/component/ButtonList.tsx
+++ src/component/ButtonList.tsx
@@ -4,9 +4,11 @@ import { v4 as uuid } from 'uuid';
 import "../App.css"
 
 let ran = false
+let ok = false
+const adress = "http://192.168.1.40:9000"
 
 const client = axios.create({
-  baseURL: "http://192.168.1.40:9000" 
+  baseURL: adress
 });
 
 const Folder = (hello:any, setAnim:any) => {
@@ -25,38 +27,40 @@ const ButtonList = () => {
   const imgRef = useRef(null)
   const titleRef = useRef(null)
  
-
-  if (!ran){client.get("/animeList").then((response) => {setAnim(response.data);ran = true})}
-    
-  return (
+  if (!ran){client.get("/animeList").then((response) => {setAnim(response.data);ran = true;ok = true})}
+  if (ran){console.log(anim)}
+  {if (ok) {return (
     <div>
       <h3 className='title' ref={titleRef}> </h3>
-      <div className='gridButton'>
+      {<div className='gridButton'>
         {anim.map((c: {cover: any, name: any , episodes: any[], isfile: boolean, anime:string, img:any}) => 
-            
+           
             <button className='But' key={uuid()} onClick={(e) => {
+
               if (c.isfile) {
-                vidRef.current.src = "http://192.168.1.40:9000/content/"+c.anime+"/"+c.name;
+                vidRef.current.src = adress + "/content/" + c.anime +"/" +c.name;
                 
                 vidRef.current.height = 720 / 2;
                 vidRef.current.width = 1280 / 2;
               }
-              else{                
+              else{
+                
+                console.log(c)                
                 Folder(c, setAnim)
                 titleRef.current.textContent = c.name
-                // imgRef.current.src = "http://192.168.1.40:9000/content/"+c.anime+"/"+c.cover;
               }
             }}>
-              {!c.isfile && <img alt='' ref={imgRef} height={195+20} width={138+20} src={"http://192.168.1.40:9000/content/"+c.name+"/"+c.cover}/>}
+              {!c.isfile && <img alt='' ref={imgRef} height={195+20} width={138+20} src={adress + "/content/" + c.name+ "/"  + c.cover}/>}
               {!c.isfile && <br/>}
               {c.name}
+              
             </button>
         )}
-        </div>
+        </div>}
       <div className='vid'><video ref={vidRef} controls={true} src="" width={0} height={0}></video></div>
     </div>
-  )
-}
+  ) } else{return <div></div>}
+}}
 
 export default ButtonList
 
